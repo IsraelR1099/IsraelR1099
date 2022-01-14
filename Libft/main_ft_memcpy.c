27041ba09ac0829a342e6d481b7fe7d6236f1cbc
Israel Rifarachi hoyos <irifarac@cbr9s4.42barcelona.com>
2022-01-14 13:01:05 +0100

    Modificacion en main memcpy

--- Libft/main_ft_memcpy.c
+++ Libft/main_ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: irifarac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 09:39:40 by irifarac          #+#    #+#             */
-/*   Updated: 2022/01/13 11:21:41 by irifarac         ###   ########.fr       */
+/*   Updated: 2022/01/14 09:51:39 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ int	main(void)
 	char	destination2[] = "como estas";
 
 	memcpy(destination, source, 4);
-	printf("El resultado es: %s\n", destination);
+	printf("\033[31;1;13mEl resultado es: %s\n\033[0m", destination);
 
 	ft_memcpy(destination2, source, 4);
-	printf("El resultado con ft_memcpy es: %s\n", destination2); 
+	printf("\033[32;1;13mEl resultado con ft_memcpy es: %s\n\033[0m", destination2); 
 	return (0);
 }
