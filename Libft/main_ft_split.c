342a32ea2919e66e9d27f6b648f011aeaee42cd4
IsraelR1099 <95863801+IsraelR1099@users.noreply.github.com>
2022-02-10 22:58:41 +0100

    acabado ftlstiter

--- Libft/main_ft_split.c
+++ Libft/main_ft_split.c
@@ -6,28 +6,45 @@
 /*   By: irifarac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 10:06:34 by irifarac          #+#    #+#             */
-/*   Updated: 2022/02/07 13:51:22 by irifarac         ###   ########.fr       */
+/*   Updated: 2022/02/10 21:10:11 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "tc.h"
 
+size_t	allocation_size(void *ptr)
+{
+	return ((size_t *)ptr)[-1];
+}
 
 int	main(void)
 {
 	char	*str = "hello!";
+	char	*str2 = "  holaa   como   ";
 	char	**ptr;
+	char	**ptr2;
+
+
+
 
 	ptr = ft_split(str, ' ');
-
+	ptr2 = ft_split(str2, ' ');
+	
+	printf("El tamanio de malloc es: %lu\n", allocation_size(ptr));
 	printf("%sresultado ptr 0 es '%s'\n%s", TC_GRN, ptr[0], TC_NRM);
 	printf("%sresultado ptr 1 es '%s'\n%s", TC_GRN, ptr[1], TC_NRM);
-	printf("%sresultado ptr 2 es '%s'\n%s", TC_GRN, ptr[2], TC_NRM);
-	printf("%sresultado ptr 3 es '%s'\n%s", TC_GRN, ptr[3], TC_NRM);
-	printf("%sresultado ptr 4 es '%s'\n%s", TC_GRN, ptr[4], TC_NRM);
-	printf("%sresultado ptr 5 es '%s'\n%s", TC_GRN, ptr[5], TC_NRM);
-//	printf("%sresultado ptr 6 es '%s'\n%s", TC_GRN, ptr[6], TC_NRM);
+//	printf("%sresultado ptr 2 es '%s'\n%s", TC_GRN, ptr[2], TC_NRM);
+
+//	printf("%sresultado ptr 2 es '%s'\n%s", TC_GRN, ptr[2], TC_NRM);
+//	printf("%sresultado ptr 3 es '%s'\n%s", TC_GRN, ptr[3], TC_NRM);
+//	printf("%sresultado ptr 4 es '%s'\n%s", TC_GRN, ptr[4], TC_NRM);
+//	printf("%sresultado ptr 5 es '%s'\n%s", TC_GRN, ptr[5], TC_NRM);
+//	printf("%sresultado ptr 6 es '%s'\n%s", TC_GRN, ptr[6], TC_NRM)
+//	;
+	printf("%sresultado ptr2 0 es '%s'\n%s", TC_GRN, ptr2[0], TC_NRM);
+	printf("%sresultado ptr2 1 es '%s'\n%s", TC_GRN, ptr2[1], TC_NRM);
+	free(ptr2);
 	free(ptr);
 
 	return (0);
