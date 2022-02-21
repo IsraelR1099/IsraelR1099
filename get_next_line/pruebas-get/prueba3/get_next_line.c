45fa31013713aa882834e69edbe689873fdbbfeb
IsraelR1099 <95863801+IsraelR1099@users.noreply.github.com>
2022-02-21 23:12:25 +0100

    mejorando get next line

--- get_next_line/pruebas-get/prueba3/get_next_line.c
+++ get_next_line/pruebas-get/prueba3/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: irifarac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 13:56:53 by irifarac          #+#    #+#             */
-/*   Updated: 2022/02/21 14:00:20 by irifarac         ###   ########.fr       */
+/*   Updated: 2022/02/21 23:06:50 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 static char	*ft_line(char const *s, char c)
 {
-	static char	*buffer = NULL;
+	//static char	*buffer = NULL;
 	char		*token;
 
-	buffer = (char *)s;
-	token = buffer;
+	//buffer = (char *)s;
+	token = ft_read_file;
 	if (!*buffer)
 	{
 		buffer = 0;
@@ -33,4 +33,47 @@ static char	*ft_line(char const *s, char c)
 	}
 	return (token);
 }
+
+static char	*ft_read_file(char *str, int fd)
+{
+	static char	*whole_str = NULL;
+	int		nbr_bytes;
+
+	if (!whole_str)
+	{
+		whole_str = (char *)malloc(sizeof(*whole_str) * (BUFFER_SIZE + 1));
+		if (!whole_str)
+			return (0);
+	}
+	while (!ft_strchr(temp_str, '\n'))
+	{
+		whole_str = ft_strjoin(whole_str, temp_str);
+	}
+	return (whole_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*temp_str;
+	int		nbr_bytes;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	temp_str = (char *)malloc(sizeof(*temp_str) * (BUFFER_SIZE + 1));
+	if (!temp_str)
+	{
+		free(temp_str);
+		return (0);
+	}
+	nbr_bytes = 1;
+	nbr_bytes = read(fd, temp_str, BUFFER_SIZE);
+	if (nbr_bytes == -1)
+	{
+		free(temp_str);
+		return (0);
+	}
+	if (nbr_bytes > 0)
+	{
+		temp_str[nbr_bytes] = '\0';
+		ft_read_file(temp_str, fd);
 
