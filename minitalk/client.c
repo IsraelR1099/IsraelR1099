319b950988cba6d07667e6649e8d5bfb41a394d8
Israel Rifarachi hoyos <irifarac@cbr9s4.42barcelona.com>
2022-05-17 14:06:05 +0200

    minitalk 2

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: irifarac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 13:22:53 by irifarac          #+#    #+#             */
-/*   Updated: 2022/05/16 20:54:48 by irifarac         ###   ########.fr       */
+/*   Updated: 2022/05/17 13:55:44 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	sign_handler2(int pid_server, char *str)
+void	sign_handler2(int pid_server, char c)
 {
 	int	bit;
 	int	position;
 
 	position = 0;
-	while (*(str + position))
+	bit = 8;
+	printf("c dentro es %c\n", c);
+	while (bit--)
 	{
-
-		bit = 8;
-		write(1, &*(str + position), 1);
-		while (bit--)
-		{
-			if (*(str + position) >> bit & 1)
-				kill(pid_server, SIGUSR1);
-			else
-				kill(pid_server, SIGUSR2);
-		}
-		position++;
+		if ( c >> bit & 1)
+			kill(pid_server, SIGUSR1);
+		else
+			kill(pid_server, SIGUSR2);
+		usleep(500);
 	}
 }
 
@@ -66,6 +62,7 @@ int	main(int counter, char **str)
 {
 //	struct sigaction	act2;
 	int					pid2;
+	int					position;
 
 	(void)counter;
 	printf("hola\n");
@@ -76,7 +73,13 @@ int	main(int counter, char **str)
 //	sigemptyset(&act2.sa_mask);
 //	signal(SIGUSR1, sign_handler2);
 //	signal(SIGUSR2, sign_handler2);
-	sign_handler2(pid2, str[2]);
+	position = 0;
+	while (*(*(str + 2) + position))
+	{
+		sign_handler2(pid2, *(*(str + 2) + position));
+		printf("c es %c\n", *(*(str + 2) + position));
+		position++;
+	}
 //	kill(pid2, SIGUSR1);
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: irifarac <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 11:35:58 by irifarac          #+#    #+#             */
-/*   Updated: 2022/05/16 20:38:52 by irifarac         ###   ########.fr       */
+/*   Updated: 2022/05/17 13:55:46 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,55 @@
 #include <unistd.h>
 //#include "ft_printf/includes/ft_printf.h"
 
+int	ft_strlen(char *str)
+{
+	int	position;
+
+	position = 0;
+	while (*(str + position))
+		position++;
+	return (position);
+}
+
 void	sign_handler(int sign)
 {
+//	unsigned char	bits;
+//	int				byte;
+	//char	*str = "mensaje recibido de SIGUSR1";
+	//char	*str2 = "mensaje recibido de SIGUSR2";
+
+//	bits = 0;
+//	byte = 8;
+//	while (byte--)
+//	{
+		//Valor 0
+/*		if (sign == SIGUSR1)
+		{
+			write(1, "0", 1);
+		//	bits = bits >> byte | 1;
+		}
+		else if (sign == SIGUSR2)
+		{
+			write(1, "1", 1);
+		//	bits = bits << 1;
+		}
+//	}
+	//write(1, &bits, 1);*/
+	static int	cont = 0;
+	static char	letter = 0;
+
 	if (sign == SIGUSR1)
-		write(1, "h", 1);
+		write(1, "1", 1);
+//		letter = letter | 1 << cont;
+	else
+		write(1, "0", 1);		
+	cont++;
+	if (cont == 8)
+	{
+		write(1, &letter, 1);
+		cont = 0;
+		letter = 0;
+	}
 }
 
 int	main(void)
@@ -31,10 +76,8 @@ int	main(void)
 	act.sa_flags = SA_RESTART;
 	printf("PID: %d\n", pid);
 	sigaction(SIGUSR1, &act, NULL);
-	kill(pid, SIGUSR1);
+	sigaction(SIGUSR2, &act, NULL);
 	while (1)
-	{
 		pause();
-	}
 	return (0);
 }
