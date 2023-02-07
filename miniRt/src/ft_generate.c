4349b2705cc899c114736dcedb54ec41d717323f
IsraelR1099 <95863801+IsraelR1099@users.noreply.github.com>
2023-02-07 20:56:44 +0100

    generando un rayo

--- miniRt/src/ft_generate.c
+++ miniRt/src/ft_generate.c
@@ -14,13 +14,24 @@
 #include "../mlx/mlx.h"
 
 
+int	ft_intersects(t_ambient *amb, t_object *obj, float x, float y, float z)
+{
+
+
 void	ft_generate(t_ambient *amb, t_object *obj, t_window *mlx)
 {
 	int	i;
 	int	j;
+	int	t;
+	float	ray_z;
+	float	len;
+	float	ray_x;
+	float	ray_y;
 	t_light	*light;
 
 	i = 0;
+	t = 100;
+	len = 0;
 	printf("amb type es %d\n", amb->type);
 	light = (t_light *)amb;
 	printf("alight->r es %d\n", light->r);
@@ -29,7 +40,14 @@ void	ft_generate(t_ambient *amb, t_object *obj, t_window *mlx)
 		j = 0;
 		while (j < 1920)
 		{
-		//	color = ft_intersects(amb, obj);
+			ray_x = x;
+			ray_y = y;
+			ray_z = -1 * t;
+			len = sqrt(ray_x * ray_xx + ray_y * ray_y + ray_z * ray_z);
+			ray_x /= len;
+			ray_y /= len;
+			ray_z /= len;
+			color = ft_intersects(amb, obj, ray_x, ray_y, ray_z);
 		//	ft_my_mlx_pxput(mlx, j, i, color);
 			ft_my_mlx_pxput(mlx, j, i, ft_color_value(amb, A));
 			j++;
