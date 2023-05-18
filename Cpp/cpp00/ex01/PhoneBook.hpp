4eb7dc0f0410ac629eecc1d4e0798bd0f203b594
IsraelR1099 <rifarachi10@gmail.com>
2023-05-18 20:59:23 +0200

    adding contact class

--- Cpp/cpp00/ex01/PhoneBook.hpp
+++ Cpp/cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: irifarac <irifarac@student42.barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 20:17:54 by irifarac          #+#    #+#             */
-/*   Updated: 2023/05/15 13:33:24 by irifarac         ###   ########.fr       */
+/*   Updated: 2023/05/18 20:15:35 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@
 #include <iostream>
 #include <string.h>
 
+class	Contact
+{
+	private:
+		std::string		first_name;
+		std::string		last_name;
+		unsigned int	phone_number;
+		std::string		dark_secret;
+};
+
 class	PhoneBook
 {
 	public:
@@ -23,5 +32,7 @@ class	PhoneBook
 		~PhoneBook();
 		void	ft_setContact(std::string line);
 		void	ft_getContact();
+	private:
+		Contact	contacts[8];
 };
 #endif
