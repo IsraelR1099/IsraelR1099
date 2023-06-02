628841d7e730be1d6d8f82759b3241d57ed2280c
Israel Rifarachi hoyos <irifarac@cbr9s3.42barcelona.com>
2023-06-02 14:08:58 +0200

    acabado puede

---
+++ miniRt/minirt-finished/src/maths/ft_vector_operation.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_vector3d_operation.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irifarac <irifarac@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 10:05:48 by irifarac          #+#    #+#             */
+/*   Updated: 2023/05/31 13:17:43 by irifarac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "maths.h"
+
+double	ft_dot_product_vect(t_vector3d vector_left, t_vector3d vector_right)
+{
+	double	x;
+	double	y;
+	double	z;
+	double	ret;
+
+	x = vector_left.x * vector_right.x;
+	y = vector_left.y * vector_right.y;
+	z = vector_left.z * vector_right.z;
+	ret = x + y + z;
+	return (ret);
+}
+
+t_vector3d	ft_vectorial_product(t_vector3d vector_left,
+t_vector3d vector_right)
+{
+	t_vector3d	crossvector;
+
+	crossvector.x = vector_left.y * vector_right.z - vector_left.z
+		* vector_right.y;
+	crossvector.y = vector_right.x * vector_left.z - vector_right.z
+		* vector_left.x;
+	crossvector.z = vector_left.x * vector_right.y - vector_right.x
+		* vector_left.y;
+	return (crossvector);
+}
+
+t_vector3d	ft_rest_vect(t_vector3d origin, t_vector3d obj)
+{
+	t_vector3d	ret;
+
+	ret.x = origin.x - obj.x;
+	ret.y = origin.y - obj.y;
+	ret.z = origin.z - obj.z;
+	return (ret);
+}
+
+t_vector3d	ft_sum_vect(t_vector3d first, t_vector3d second)
+{
+	t_vector3d	ret;
+
+	ret.x = first.x + second.x;
+	ret.y = first.y + second.y;
+	ret.z = first.z + second.z;
+	return (ret);
+}
+
+t_vector3d	ft_product_vect_scalar(t_vector3d vector, double scalar)
+{
+	t_vector3d	ret;
+
+	ret.x = vector.x * scalar;
+	ret.y = vector.y * scalar;
+	ret.z = vector.z * scalar;
+	return (ret);
+}
---
+++ miniRt/minirt-finished/src/utilities/ft_hit_point.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_hit_point.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irifarac <irifarac@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 11:11:16 by irifarac          #+#    #+#             */
+/*   Updated: 2023/05/31 09:27:31 by irifarac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shaderec.h"
+
+t_point3d	ft_hit_point(t_ray *ray, double t)
+{
+	t_vector3d	b;
+	t_vector3d	sum;
+	t_vector3d	origin;
+	t_point3d	ret;
+
+	origin.x = ray->origin.x;
+	origin.y = ray->origin.y;
+	origin.z = ray->origin.z;
+	b = ft_product_vect_scalar(ray->direction, t);
+	sum = ft_sum_vect(origin, b);
+	ret.x = sum.x;
+	ret.y = sum.y;
+	ret.z = sum.z;
+	return (ret);
+}
