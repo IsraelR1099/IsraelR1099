ce84a29468f3a77d50e30c2a43f45ea7591277db
Israel Rifarachi hoyos <irifarac@cbr8s3.42barcelona.com>
2023-06-30 13:53:47 +0200

    cpp05 ex00 changing catch block

--- Cpp/cpp05/ex00/main.cpp
+++ Cpp/cpp05/ex00/main.cpp
@@ -6,12 +6,20 @@
 /*   By: irifarac <irifarac@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 12:20:35 by irifarac          #+#    #+#             */
-/*   Updated: 2023/06/30 13:44:41 by irifarac         ###   ########.fr       */
+/*   Updated: 2023/06/30 13:53:22 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+//Hay dos formas de capturar un valor de una clase que hereda de
+//std::exception: by value catch (std::out_of_range) o by reference
+//catch (const std::out_of_range &obj).
+//Cuando capturamos el objeto by value estamos creando una copia de ese objeto
+//y todos los cambios que se hagan quedaran dentro del bloque catch. En cambio,
+//si capturamos la excepcion por referencia los cambios que hagamos quedaran
+//grabados en la excepcion misma. En general es mejor capturar la excepcion por
+//referencia, ya que evitamos copias innecesarias
 
 int	main(void)
 {
@@ -22,9 +30,9 @@ int	main(void)
 	{
 		Bureaucrat	test2("hola2", 1000);
 	}
-	catch (std::out_of_range)
+	catch (const std::out_of_range &exception) //catching by reference
 	{
-		std::cout << "Error out of range\n" << std::endl;
+		std::cerr << "Error out of range\n" << std::endl;
 	}
 
 	try
@@ -33,7 +41,7 @@ int	main(void)
 	}
 	catch (const Bureaucrat::GradeTooLowException &exception)
 	{
-		std::cout << "Exception\n" << std::endl;
+		std::cerr << "Exception\n" << std::endl;
 	}
 	std::cout << "name es: " << test.getName() << std::endl;
 	std::cout << "grade es: " << test.getGrade() << std::endl;
