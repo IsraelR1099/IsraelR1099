841761dc17eb13daf776b5f66d6f09a7556b0744
IsraelR1099 <rifarachi10@gmail.com>
2023-07-16 20:52:55 +0200

    finishing ex02 cpp05

--- Cpp/cpp05/ex03/PresidentialPardonForm.hpp
+++ Cpp/cpp05/ex03/PresidentialPardonForm.hpp
@@ -1,35 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: irifarac <irifarac@student42.barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/06 13:17:20 by irifarac          #+#    #+#             */
-/*   Updated: 2023/07/15 18:53:28 by irifarac         ###   ########.fr       */
+/*   Created: 2023/07/06 13:31:11 by irifarac          #+#    #+#             */
+/*   Updated: 2023/07/07 12:26:48 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SHRUBBERYCREATIONFORM_HPP
-#define SHRUBBERYCREATIONFORM_HPP
+#ifndef PRESIDENTIALPARDONFORM_HPP
+# define PRESIDENTIALPARDONFORM_HPP
 
-# include "AForm.hpp"
-# include <fstream>
+#include "AForm.hpp"
 
-//with the override keyword we ensure that this function actually overrides a
-//virtual function in the base class.
-
-class	ShrubberyCreationForm : public AForm
+class	PresidentialPardonForm : public AForm
 {
 	public:
-		ShrubberyCreationForm(void);
-		ShrubberyCreationForm(std::string target);
-		ShrubberyCreationForm(const ShrubberyCreationForm &obj);
-		~ShrubberyCreationForm(void);
-		ShrubberyCreationForm	&operator=(const ShrubberyCreationForm &obj);
+		PresidentialPardonForm(void);
+		PresidentialPardonForm(std::string target);
+		PresidentialPardonForm(const PresidentialPardonForm &obj);
+		~PresidentialPardonForm(void);
+		PresidentialPardonForm &operator=(const PresidentialPardonForm &obj);
 		void	execute(Bureaucrat const &executor) const;
 		bool	checkRequirement(const Bureaucrat &executor) const;
 	private:
-		const std::string	m_target;
+		const std::string m_target;
 };
 #endif
--- Cpp/cpp05/ex03/RobotomyRequestForm.hpp
+++ Cpp/cpp05/ex03/RobotomyRequestForm.hpp
@@ -1,32 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: irifarac <irifarac@student42.barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/06 13:17:20 by irifarac          #+#    #+#             */
-/*   Updated: 2023/07/15 18:53:28 by irifarac         ###   ########.fr       */
+/*   Created: 2023/07/06 13:28:01 by irifarac          #+#    #+#             */
+/*   Updated: 2023/07/07 12:27:14 by irifarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SHRUBBERYCREATIONFORM_HPP
-#define SHRUBBERYCREATIONFORM_HPP
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
 
 # include "AForm.hpp"
-# include <fstream>
 
-//with the override keyword we ensure that this function actually overrides a
-//virtual function in the base class.
-
-class	ShrubberyCreationForm : public AForm
+class	RobotomyRequestForm : public AForm
 {
 	public:
-		ShrubberyCreationForm(void);
-		ShrubberyCreationForm(std::string target);
-		ShrubberyCreationForm(const ShrubberyCreationForm &obj);
-		~ShrubberyCreationForm(void);
-		ShrubberyCreationForm	&operator=(const ShrubberyCreationForm &obj);
+		RobotomyRequestForm(void);
+		RobotomyRequestForm(std::string target);
+		RobotomyRequestForm(const RobotomyRequestForm &obj);
+		~RobotomyRequestForm(void);
+		RobotomyRequestForm	&operator=(const RobotomyRequestForm &obj);
 		void	execute(Bureaucrat const &executor) const;
 		bool	checkRequirement(const Bureaucrat &executor) const;
 	private:
