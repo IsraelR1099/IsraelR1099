bcfad33d64f7f5e3aaf0684d75e21ada933e1866
IsraelR1099 <rifarachi10@gmail.com>
2023-08-15 21:03:43 +0200

    studying about cpp09 and containers

---
+++ Cpp/cpp09/cpp_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irifarac <irifarac@student42.barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 20:43:01 by irifarac          #+#    #+#             */
+/*   Updated: 2023/08/15 20:53:28 by irifarac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <string>
+# include <map>
+
+//En los contenedores de c++ hay tres clases de contenedores: los asociativos,
+//los secuenciales y los adaptadores. Los asociativos son aquellos que
+//almacenan sus elementos de forma ordenada, y que permiten el acceso a los
+//mismos a traves de una clave (std::map, std::set, std::multimap,
+//std::multiset) (std::multiset y std::multimap, son similares a map y set,
+// con la diferencia que aceptan repeticiones en sus inputs. Los secuenciales
+//son aquellos que almacenan sus elementos de forma secuencial, como
+//una array (std::vector, std::list, std::deque). Los adaptativos son aquellos
+//que estan hechos para un uso especifico (std::stack, std::queue, std::priority_queue).
