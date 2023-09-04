b4e355591ae764ca301a2d5a91d9399b0fb2939d
Israel Rifarachi hoyos <irifarac@car11s4.42barcelona.com>
2023-09-04 12:03:02 +0200

    script for mariadb

---
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,44 @@
+#!bin/bash
+
+# First we check if the database already exists, if so, we do not do anything.
+# In the contrary case, we execute "mysql_secure_installation"
+# The flags of mysql are: -s silen, -f keep going if there is an error and -u
+# uses the user specified (in this case is the user root)
+# After setting the configuration of "mysql_secure_installation", we create the
+# database with utf8 which makes our database suitable for multilingual
+# applications.
+# utf8_unicode_ci means that our database is case-insensitive and
+# accent-insensitive, which is suitable with diverse languages.
+
+/etc/init.d/mysql start
+
+if [ -d /var/lib/mysql/$MYSQL_DATABASE]
+then
+	echo "Database already exists"
+else
+mysql -sfu root <<EOSQL
+-- set root password
+UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWD') WHERE User='root';
+-- delete anonymous users
+DELETE FROM mysql.user WHERE User='';
+-- delete remote root capabilities
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost',
+'127.0.0.1', '::1');
+-- drop database 'test'
+DROP DATABASE IF EXISTS test;
+-- make sure there are lingering permissions to it
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+-- create database for wordpress
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8
+COLLATE utf8_unicode_ci;
+-- create user for wordpress
+CREATE USER '{WP_NAME}'@'localhost' IDENTIFIED BY '{WP_PASS}';
+-- grant privileges to the new user
+GRANT ALL PRIVILEGES ON wordpress.* TO ${WP_NAME}@'localhost' IDENTIFIED BY
+${WP_PASS};
+-- make changes inmediately
+FLUSH PRIVILEGES;
+EOSQL
+fi
+
+/etc/init.d/mysql stop
