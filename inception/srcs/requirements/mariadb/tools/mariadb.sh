240b11a72cbda246ce25f1af490c2cde9988a5d7
IsraelR1099 <rifarachi10@gmail.com>
2023-09-05 20:56:43 +0200

    it left to check if it works mariadb

--- inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -10,35 +10,51 @@
 # utf8_unicode_ci means that our database is case-insensitive and
 # accent-insensitive, which is suitable with diverse languages.
 
+# Function to handle errors
+handle_error()
+{
+	local exit_code=$1
+	local error_message="$2"
+	echo "Error: $error_message"
+	exit $exit_code
+}
+
 /etc/init.d/mysql start
 
 if [ -d /var/lib/mysql/$MYSQL_DATABASE]
 then
 	echo "Database already exists"
 else
-mysql -sfu root <<EOSQL
--- set root password
-UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWD') WHERE User='root';
--- delete anonymous users
-DELETE FROM mysql.user WHERE User='';
--- delete remote root capabilities
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost',
-'127.0.0.1', '::1');
--- drop database 'test'
-DROP DATABASE IF EXISTS test;
--- make sure there are lingering permissions to it
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
--- create database for wordpress
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8
-COLLATE utf8_unicode_ci;
--- create user for wordpress
-CREATE USER '{WP_NAME}'@'localhost' IDENTIFIED BY '{WP_PASS}';
--- grant privileges to the new user
-GRANT ALL PRIVILEGES ON wordpress.* TO ${WP_NAME}@'localhost' IDENTIFIED BY
-${WP_PASS};
--- make changes inmediately
-FLUSH PRIVILEGES;
+
+	if mysql -sfu root <<EOSQL
+	-- set root password
+	UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWD') WHERE User='root';
+	-- delete anonymous users
+	DELETE FROM mysql.user WHERE User='';
+	-- delete remote root capabilities
+	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost',
+	'127.0.0.1', '::1');
+	-- drop database 'test'
+	DROP DATABASE IF EXISTS test;
+	-- make sure there are lingering permissions to it
+	DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+	-- create database for wordpress
+	CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8
+	COLLATE utf8_unicode_ci;
+	-- create user for wordpress
+	CREATE USER ${WP_USER}'@'localhost' IDENTIFIED BY ${WP_PASSWD};
+	-- grant privileges to the new user
+	GRANT ALL PRIVILEGES ON wordpress.* TO ${WP_USER}@'localhost' IDENTIFIED BY
+	${WP_PASSWD};
+	-- make changes inmediately
+	FLUSH PRIVILEGES;
 EOSQL
+	then
+		echo "Database created successfully"
+	else
+		handle_error 1 "Error creating database"
+	fi
 fi
 
+echo "MariaDB setup script finished."
 /etc/init.d/mysql stop
