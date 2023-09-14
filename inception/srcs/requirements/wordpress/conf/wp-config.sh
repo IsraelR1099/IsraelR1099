db01e3fd4fc28053e916c65eca3c87f3f8d5ac45
IsraelR1099 <rifarachi10@gmail.com>
2023-09-14 23:07:29 +0200

    socker problems

--- inception/srcs/requirements/wordpress/conf/wp-config.sh
+++ inception/srcs/requirements/wordpress/conf/wp-config.sh
@@ -1,73 +1,41 @@
-##!/bin/sh
-#
-# #First we check if the wp-config.php file exists
-# #If it does not exist, we create it
-# #If it does exist, we do nothing
-# #wp config create: this command creates the wp-config.php file
-# #wp core install: this command installs WordPress
-# #wp user create: this command creates a new user
-#
-#sleep 10
-#
-#if [ ! -f ./wp-config.php ]
-#then
-#	wp config create --allow-root \
-#		--dbname=${MYSQL_DATABASE} \
-#		--dbuser=${MYSQL_USER} \
-#		--dbpass=${MYSQL_PASSWORD} \
-#		--dbhost=mariadb:3306 \
-#		#--path='/var/www/html'
-#	sleep 2
-#	wp core install --url=$DOMAIN_NAME \
-#		--title=$SITE_TITLE \
-#		--admin_user=$ADMIN_USER \
-#		--admin_password=$ADMIN_PASSWORD \
-#		--admin_email=$ADMIN_EMAIL \
-#		--allow-root \
-#		#--path='/var/www/html'
-#	wp user create --allow-root \
-#		$USER_NAME \
-#		$USER_EMAIL \
-#		--user_pass=$USER_PASSWORD \
-#		--role=author \
-#		#--path='/var/www/html'
-#fi
-#
-#if [ ! -d /run/php ]
-#then
-#	mkdir -p /run/php
-#	chmod 755 /run/php
-#fi
-#
-## We start the php-fpm service in the foreground
-#/usr/sbin/php-fpm7.3 -F
+#!/bin/bash
 
-if [ -f ./wp-config.php ]
+ #First we check if the wp-config.php file exists
+ #If it does not exist, we create it
+ #If it does exist, we do nothing
+ #wp config create: this command creates the wp-config.php file
+ #wp core install: this command installs WordPress
+ #wp user create: this command creates a new user
+
+sleep 10
+
+if [ ! -e /var/www/wordpress/wp-config.php ]
 then
-	echo "Wordpress already exists"
-else
 	wp core download --allow-root
 	wp config create --allow-root \
 		--dbname=${MYSQL_DATABASE} \
 		--dbuser=${MYSQL_USER} \
 		--dbpass=${MYSQL_PASSWORD} \
-		--dbhost=mariadb:3306 \
-		#--path='/var/www/html'
+		--dbhost=mariadb
 	sleep 2
 	wp core install --url=$DOMAIN_NAME \
 		--title=$SITE_TITLE \
 		--admin_user=$ADMIN_USER \
 		--admin_password=$ADMIN_PASSWORD \
 		--admin_email=$ADMIN_EMAIL \
-		--allow-root \
-		#--path='/var/www/html'
+		--allow-root
 	wp user create --allow-root \
 		$USER_NAME \
 		$USER_EMAIL \
 		--user_pass=$USER_PASSWORD \
-		--role=author \
-		#--path='/var/www/html'
-	wp theme install twentysixteen --activate --allow-root
+		--role=author
 fi
 
-/usr/sbin/php-fpm7.3 -F;
+if [ ! -d /run/php ]
+then
+	mkdir -p /run/php
+	chmod 755 /run/php
+fi
+
+# We start the php-fpm service in the foreground
+/usr/sbin/php-fpm7.3 -F
