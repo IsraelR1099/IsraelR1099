5640df729f5316d5de57c43d787f89daf5aed566
Israel Rifarachi hoyos <irifarac@car11s3.42barcelona.com>
2023-12-22 14:11:24 +0100

    adding friendship class in models.py

--- transcendence/back/src/user/models.py
+++ transcendence/back/src/user/models.py
@@ -2,9 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-class Users(models.Model):
+class   Users(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birth_date = models.DateField()
     username = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
+    created_date_time = models.DateField()
 
     def __str__(self):
-        return (f"{self.username}")
+        return (f"{self.id}: {self.username}")
+
+class   Friendship_Request(models.Model):
+    requester_id = models.ForeignKey(
+            Users, on_delete=models.CASCADE, related_name="requester_id")
+    addressee_id = models.ForeignKey(
+            Users, on_delete=models.CASCADE, related_name="addressee_id")
+    created_date_time = models.DateField()
+
+    def __str__(self):
+        return (f"user id requester: {self.requester_id} to {self.addressee_id}")
--- transcendence/back/src/user/views.py
+++ transcendence/back/src/user/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -16,31 +17,31 @@ def index(request):
 @csrf_exempt
 def login_view(request: HttpRequest) -> JsonResponse:
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         # authenticate user
         user = authenticate(request, username=username, password=password)
         # check if authentication successful
         if user is not None:
             login(request, user)
             data = {
-                "message": "Login successful.",
-                "status": "success",
-                "username": username,
-                "password": password,
+                    "id": user.id,
+                    "username": user.username,
+                    "status": "online",
+                    "created_at": user.date_joined.isoformat(),
+                    "updated_at": user.last_login.isoformat(),
+                    "code": "200",
                     }
             return (JsonResponse(data))
         else:
-            data = {
-                "message": "Invalid username and/or password.",
-                "status": "error",
-                "username": username,
-                "password": password,
-                }
+            data =  {
+                    "error": "User does not exist.",
+                    "code": "404",
+                    }
             return (JsonResponse(data))
     return (render(request, "user/login.html"))
 
-@csrf_exempt
+@login_required
 def logout_view(request: HttpRequest) -> JsonResponse:
     logout(request)
     data = {
@@ -49,6 +50,7 @@ def logout_view(request: HttpRequest) -> JsonResponse:
             }
     return (JsonResponse(data))
 
+@csrf_exempt
 def register_user(request: HttpRequest) -> JsonResponse:
     form = UserCreationForm()
     if request.method == "POST":
@@ -56,23 +58,38 @@ def register_user(request: HttpRequest) -> JsonResponse:
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password1")
+            password1 = form.cleaned_data.get("password1")
+            password2 = form.cleaned_data.get("password2")
             # authenticate user
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password1)
             login(request, user)
             data = {
-                "message": "Registration successful.",
-                "status": "success",
+                    "id": user.id,
+                    "username": user.username,
+                    "status": "online",
+                    "created_at": user.date_joined.isoformat(),
+                    "updated_at": user.last_login.isoformat(),
+                    "code": "201",
                     }
+
             return (JsonResponse(data))
         else:
-            error_message = form.errors.as_json()
-            data = {
-                    "message": "Registration failed.",
-                    "status": "error",
-                    "error_message": error_message,
-                    }
-            return (JsonResponse(data))
+            # Check if username is taken
+            if "username" in form.errors:
+                username_errors = form.errors["username"]
+                user_exists_error = "A user with that username already exists." in username_errors
+                data = {
+                        "error": username_errors,
+                        "code": "409",
+                        }
+                return (JsonResponse(data))
+            else:
+                error_message = form.errors.as_json()
+                data = {
+                        "error_message": error_message,
+                        "code": "422",
+                        }
+                return (JsonResponse(data))
     return (render(request, "user/register.html", {
         "form": form
         }))
