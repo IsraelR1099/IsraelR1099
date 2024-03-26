d33f1f620eb8df8ecfa60735cbf09f3ce85a038e
IsraelR1099 <rifarachi10@gmail.com>
2024-03-26 21:07:21 +0100

    trying to change my front

---
+++ transcendence/front/changing/js/login.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const loginForm = document.getElementById('loginForm');
+	async function loginUser(username, password) {
+		try {
+			const response = await fetch('/api/user/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ username, password }),
+			});
+
+			const data = await response.json();
+			if (data.error) {
+				console.error("Login failed: ", data.error);
+				alert (data.error['__all__'][0]);
+			}
+			else if (response.ok) {
+				console.log("Login successful");
+				localStorage.setItem('userData', JSON.stringify(data));
+				await fetchAccountData(data.id);
+			}
+			else {
+				console.error("Login failed: ", data);
+				alert ("Login failed");
+			}
+		} catch (error) {
+			console.error("Login failed: ", error);
+			alert ("Login failed");
+		}
+	}
+
+	loginForm.addEventListener('submit', async function(event) {
+		event.preventDefault();
+		const username = document.getElementById('username').value;
+		const password = document.getElementById('password').value;
+		await loginUser(username, password);
+	}
+});
---
+++ transcendence/front/changing/js/register.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+	const registerForm = document.getElementById('registerForm');
+	async function registerUser(email, username, password1, password2) {
+		try {
+			const response = await fetch('/api/user/register/', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					email,
+					username,
+					password1,
+					password2,
+				}),
+			});
+
+			const data = await response.json();
+			if (response.ok) {
+				console.log(data);
+				localStorage.setItem('userData', JSON.stringify(data));
+				await fetchAccountData(data.id);
+			}
+			else {
+				console.error("Register failed: ", data.error);
+			}
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	}
+
+	registerForm.addEventListener('submit', async function(event) {
+		event.preventDefault();
+		const email = document.getElementById('inputEmail').value;
+		const username = document.getElementById('inputUsername').value;
+		const password1 = document.getElementById('inputPassword1').value;
+		const password2 = document.getElementById('inputPassword2').value;
+		await registerUser(email, username, password1, password2);
+	});
+});
